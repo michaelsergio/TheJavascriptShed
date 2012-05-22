196451b8cd68e1fd4e10f9cd6e30b45893054076
Michael Sergio <mikeserg@gmail.com>
2012-05-22 14:33:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thejavascriptshed::Application.config.secret_token = '0bb59396934a3fbc7e5503507d461e3b6fe7af6b9694290e7c7239ecedbef0e7c62b8fa57de8845228eba6165c048fea435d0cbd9e3e19382f1decf07aa7f9b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Thejavascriptshed::Application.config.session_store :cookie_store, key: '_thejavascriptshed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Thejavascriptshed::Application.config.session_store :active_record_store
